99590961e5f55793dc82775921cd38f10b1a9fec
itslululu <84468266+itslululu@users.noreply.github.com>
2024-01-20 16:03:23 -0800

    feature: google maps component initialization

--- carpool-front-end/src/App.tsx
+++ carpool-front-end/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Login from "./pages/Login.tsx"
+import Map from "./pages/Map.tsx"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/map" element={<Map/>}/>
         </Routes>
       </BrowserRouter>
       
