447f2d8a16382e8912b860b7edad3d6b87f02be8
Ben Lu <benjamincanada12@gmail.com>
2024-01-20 18:52:10 -0800

    feature: auth0 set-up complete

--- carpool-front-end/src/App.tsx
+++ carpool-front-end/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Map from "./pages/Map.tsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Start from './pages/Start.tsx'import { Home } from './pages/Home.tsx'
+import { Home } from './pages/Home.tsx'
 
 function App() {
 
@@ -9,9 +9,8 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path="/" index element={<Home/>}/>
           <Route path="/map" element={<Map/>}/>
-          <Route index element={<Start/>}/>
+          <Route path="/" index element={<Home/>}/>
         </Routes>
       </BrowserRouter>
     </div>
